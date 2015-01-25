d4060985ef865d85e190d3317744ec3a543c672c
Dimitar Dyankov <dimitar.dyankov@gifts.com>
2015-01-24 21:56:44 -0800

    fenwick tree

---
+++ DS/FenwickTree.scala
@@ -0,0 +1,51 @@
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * Created by dimitar on 1/23/15.
+ */
+object FenwickTree {
+
+  def build(arr: Array[Int]): ArrayBuffer[Int] = {
+
+    val fanwick = new ArrayBuffer[Int](arr.length+1)
+
+    (0 to arr.length).foreach{r=> fanwick.append(0)}
+    for (i<-0 until arr.length) {
+      update(fanwick, i, arr(i))
+    }
+    fanwick
+  }
+
+  def update(fanwick: ArrayBuffer[Int], index: Int, num: Int): Unit = {
+    var idx = index + 1
+    while (idx <= fanwick.length) {
+      fanwick(idx) += num
+      idx += idx & (-idx)
+    }
+  }
+
+  def getSum(fanwick: ArrayBuffer[Int], start: Int, end: Int): Int = {
+    var leftSum = 0
+    var rightSum = 0
+
+    //find left
+    var left = start+1
+    while (left > 0) {
+      leftSum += fanwick(left)
+      left -= left & (-left)
+    }
+
+    //fid right
+    var right = end+1
+    while (right > 0) {
+      rightSum += fanwick(right)
+      right -= right & (-right)
+    }
+
+    if (start > 0)
+      rightSum - leftSum
+    else
+      rightSum
+  }
+
+}
